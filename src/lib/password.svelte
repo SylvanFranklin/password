e0ab36d117966130ca866b5da6addcccfe9fe2d1
Sylvan Franklin <sylvanfranklin@icloud.com>
2024-02-22 12:20:49 -0500

    added password array

---
+++ src/lib/password.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let password: { username: string; password: string };
+</script>
+
+<div class="flex flex-col w-1/2 p-4 bg-gray-200 rounded-lg shadow-lg my-4">
+    <p class="text-xl font-bold">Username: {password.username}</p>
+    <p class="text-xl font-bold">Password: {password.password}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,30 @@
+<script lang="ts">
+    import Password from "$lib/password.svelte";
+
+    interface PasswordObject {
+        username: string;
+        password: string;
+    }
+
+    const passwords: PasswordObject[] = [
+        {
+            username: "test",
+            password: "test",
+        },
+        {
+            username: "test2",
+            password: "test2",
+        },
+    ];
+</script>
+
 <main class="flex flex-col w-full h-full mt-20 items-center">
     <h1
         class="text-5xl font-bold font-mono p-4 bg-blue-400 text-white rounded-lg shadow-lg"
     >
         PASSWORD MANAGER
     </h1>
+    {#each passwords as password}
+        <Password {password} />
+    {/each}
 </main>
