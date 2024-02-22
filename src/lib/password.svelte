bcd6c4fcf5f3c68328c279803ad338bc3b455278
Sylvan Franklin <sylvanfranklin@icloud.com>
2024-02-22 12:53:36 -0500

    added a better looking (non functional form)

--- src/lib/password.svelte
+++ src/lib/password.svelte
@@ -1,21 +1,43 @@
 <script lang="ts">
     export let password: { username: string; password: string };
+    // thought that index, might be useful in numbering ??
+    export let index: number;
 </script>
 
 <div
-    class="flex bg-slate-600/10 shadow-lg w-full flex-col text-gray-200 p-4 gap-6"
+    class="flex bg-slate-600/10 shadow-lg w-full flex-col text-gray-200 p-4 gap-2 rounded-lg font-mono"
 >
-    <p class="text-xl font-bold">
-        Username:
-        <span class="rounded-md p-2 bg-slate-200/10">
-            {password.username}
-        </span>
-    </p>
-    <p class="text-xl font-bold">
-        Password:
+    <p class="text-xl font-bold w-32">Username</p>
+    <span class="rounded-md p-2 bg-slate-200/10 flex flex-row">
+        {password.username}
+        <button class="ml-auto">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                viewBox="0 0 24 24"
+                ><path
+                    fill="currentColor"
+                    d="M9 18q-.825 0-1.412-.587T7 16V4q0-.825.588-1.412T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.587 1.413T18 18zm-4 4q-.825 0-1.412-.587T3 20V6h2v14h11v2z"
+                /></svg
+            >
+        </button>
+    </span>
 
-        <span class="rounded-md p-2 bg-slate-200/10">
-            {password.password}
-        </span>
-    </p>
+    <p class="text-xl font-bold">Password</p>
+    <span class="rounded-md p-2 bg-slate-200/10 flex flex-row">
+        {password.password}
+        <button class="ml-auto">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                viewBox="0 0 24 24"
+                ><path
+                    fill="currentColor"
+                    d="M9 18q-.825 0-1.412-.587T7 16V4q0-.825.588-1.412T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.587 1.413T18 18zm-4 4q-.825 0-1.412-.587T3 20V6h2v14h11v2z"
+                /></svg
+            >
+        </button>
+    </span>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,13 +20,39 @@
 
 <main class="flex flex-col w-full h-full mt-20 items-center">
     <h1
-        class="text-5xl font-bold font-mono p-4 bg-blue-400 text-white rounded-lg shadow-lg"
+        class="text-3xl font-bold font-mono p-2 bg-blue-400 text-white rounded-lg shadow-lg"
     >
         PASSWORD MANAGER
     </h1>
+
+    <form
+    class=" w-2/3 flex bg-slate-600/10 shadow-lg flex-col text-gray-200 p-4 gap-2 rounded-lg font-mono mt-10"
+    >
+    <h1 class="font-mono text-xl text-white mx-auto outline-none">
+    Add Password
+    </h1>
+
+    <span class="gap-3 flex flex-col">
+    <input
+    type="text"
+    class="bg-slate-200/10 p-2 rounded-lg outline-none"
+    placeholder="username"
+    />
+    <input
+    type="text"
+    class="bg-slate-200/10 p-2 rounded-lg outline-none"
+    placeholder="password"
+    />
+    <button
+    class="px-4 py-2 rounded-md bg-orange-400 w-min mx-auto hover:scale-95 duration-100 shadow-md"
+    type="submit">add</button
+    >
+    </span>
+    </form>
+
     <ol class="grid w-2/3 mt-20 gap-4">
-        {#each passwords as password}
-            <Password {password} />
+        {#each passwords as password, index}
+            <Password {password} index={index + 1} />
         {/each}
     </ol>
 </main>
