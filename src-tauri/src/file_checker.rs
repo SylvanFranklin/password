6c8f3c96a595b923b98b186825f01ee95c6bf517
ewanpedersenUVM <ejpeders@uvm.edu>
2024-02-27 12:28:22 -0500

    moved file location

--- src-tauri/src/file_checker.rs
+++ src-tauri/src/file_checker.rs
@@ -1,22 +1,27 @@
 use std::fs::File;
 use std::path::Path;
+use std::env;
 use std::io::prelude::*;
 
 pub fn check_if_json_file_exists() -> bool {
-    let path = Path::new("passwords.json");
-    let hash_path: &Path = Path::new("hash.txt");
-    println!("JSON DB: {}, HASH DOC: {}", path.exists(), hash_path.exists()); // Added closing parenthesis
-    (path.exists() && hash_path.exists())
+    let home_dir = env::var("HOME").expect("Failed to get home directory");
+    let path_str = format!("{}/Desktop/PasswordManager/passwords.json", &home_dir);
+    let path = Path::new(&path_str);
+    let hash_path_str = format!("{}/Desktop/PasswordManager/hash.txt", &home_dir);
+    let hash_path: &Path = Path::new(&hash_path_str);
+    println!("JSON DB: {}, HASH DOC: {}", path.exists(), hash_path.exists());
+    path.exists() && hash_path.exists()
 }
 
 pub fn create_if_not_exists() {
     if !check_if_json_file_exists() {
         print!("Creating json file passwords.json");
-        let mut file: File = File::create("passwords.json").unwrap();
+        let home_dir = env::var("HOME").expect("Failed to get home directory");
+        let mut file: File = File::create(&format!("{}/Desktop/PasswordManager/passwords.json", home_dir)).unwrap();
         file.write_all(b"hello world!").unwrap();
 
         print!("Creating hash file hash.txt");
-        let mut hash_file: File = File::create("hash.txt").unwrap();
+        let mut hash_file: File = File::create(&format!("{}/Desktop/PasswordManager/hash.txt", home_dir)).unwrap();
         hash_file.write_all(b"hello world!").unwrap();
     } else {
         print!("File already exists");
