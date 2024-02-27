c3c0a901ade963ba78ece643dbc6b8bf56ce3b74
ewanpedersenUVM <ejpeders@uvm.edu>
2024-02-27 13:58:24 -0500

    minor changes

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -5,7 +5,7 @@
 
     // this open function is just for getting into the password interface
     export let open: Function;
-    let encryptionKey: string;
+    let encryptionKey: "";
 </script>
 
 <main class="grid gap-4 mt-10 w-2/3">
@@ -18,7 +18,7 @@
     <button
         class="p-2 w-full shadow-md rounded-md text-xl font-mono text-white bg-slate-600/10 hover:brightness-110 hover:scale-[.99] duration-100"
         on:click={() => {
-            invoke("fileCheck", { encryptionKey });
+            invoke("file_check", {encryptionKey});
             open();
         }}
     >
