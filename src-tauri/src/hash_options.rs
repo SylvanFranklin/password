c17c7f45f53d3269c1c4fcc420c8ca066038c305
ewanpedersenUVM <ejpeders@uvm.edu>
2024-02-27 13:24:08 -0500

    added hashing backend

--- src-tauri/src/hash_options.rs
+++ src-tauri/src/hash_options.rs
@@ -1,2 +1,38 @@
-use sha566::Sha566;
+use std::env;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::{self, BufRead};
+use sha256::digest;
 
+fn write_hash_to_file(password: &str) {
+    // take password and hash it using sha256, replace previous hash in file
+    let hash = sha256::digest(password.as_bytes());
+    let home_dir = env::var("HOME").unwrap();
+    let dir_path = format!("{}/Desktop/PasswordManager", home_dir);
+    let path = format!("{}/hash.txt", dir_path);
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(&path)
+        .unwrap();
+    file.write_all(hash.as_bytes()).unwrap();
+    println!("Hash written to file");
+}
+
+fn compare_password(password: &str) -> bool {
+    let home_dir = env::var("HOME").unwrap();
+    let dir_path = format!("{}/Desktop/PasswordManager", home_dir);
+    let path = format!("{}/hash.txt", dir_path);
+
+    let file = File::open(&path).unwrap();
+    let reader = io::BufReader::new(file);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        let password = sha256::digest(password.as_bytes());
+        if line == password {
+            return true;
+        }
+    }
+    return false;
+}
